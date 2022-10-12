33f72e177ba17c248e331258d8413172656fe1d3
iwright5123 <iwright5123@tamu.edu>
2022-10-12 10:07:11 -0500

    ServerView start
    Starting of ServView, but Kevin has done things.. I'll commit and then look at what he did

--- src/main/java/edu/tamu/spinnstone/ui/screens/ServerScreen.java
+++ src/main/java/edu/tamu/spinnstone/ui/screens/ServerScreen.java
@@ -16,11 +16,20 @@ import java.io.IOException;
 
 import edu.tamu.spinnstone.ui.Window;
 
+
+/*
+ * @author ServerScreen
+ * This class is the UI for the Server
+ */
 public class ServerScreen {
   Window window;
   JPanel screen;
   BufferedImage background;
 
+  /*
+   * @param window  This connects the window and the Jpanel screen
+   * @throws IOException
+   */
   public ServerScreen(Window window) throws IOException {
     this.window = window;
     screen = new JPanel();
@@ -38,35 +47,43 @@ public class ServerScreen {
     panel.setBackground(Color.LIGHT_GRAY);
 
     JButton cheesePizzaButton = new JButton("Cheese");
-    cheesePizzaButton.setPreferredSize(new Dimension(250, 50));
-    cheesePizzaButton.setFont(new Font("Arial", Font.PLAIN, 18));
-    cheesePizzaButton.setBackground(Color.BLACK);
-    cheesePizzaButton.setForeground(Color.WHITE);
+    generateButton(cheesePizzaButton, 100, 100);
 
     JButton oneToppingPizzaButton = new JButton("1 Topping");
-    oneToppingPizzaButton.setPreferredSize(new Dimension(250, 50));
-    oneToppingPizzaButton.setFont(new Font("Arial", Font.PLAIN, 18));
-    oneToppingPizzaButton.setBackground(Color.BLACK);
-    oneToppingPizzaButton.setForeground(Color.WHITE);
+    generateButton(oneToppingPizzaButton, 350, 100);
 
     JButton BYOpizzaButton = new JButton("Build Your Own");
-    BYOpizzaButton.setPreferredSize(new Dimension(250, 50));
-    BYOpizzaButton.setFont(new Font("Arial", Font.PLAIN, 18));
-    BYOpizzaButton.setBackground(Color.BLACK);
-    BYOpizzaButton.setForeground(Color.WHITE);
+    generateButton(BYOpizzaButton, 600, 100);
 
+    JButton checkOutButton = new JButton("Checkout");
+    generateButton(checkOutButton, 100, 600);
+    
     JPanel greenPanelCol = new JPanel();
     greenPanelCol.setBackground(Color.green);
-    greenPanelCol.setBounds(0, 0, window.screenWidth, window.screenHeight);
     greenPanelCol.setPreferredSize(new Dimension(100, window.screenHeight));
+    greenPanelCol.setLocation(0, 0);
 
-    
+    panel.add(greenPanelCol);
     panel.add(cheesePizzaButton);
     panel.add(oneToppingPizzaButton);
     panel.add(BYOpizzaButton);
-    panel.add(greenPanelCol);
+    panel.add(checkOutButton);
     f.add(panel);
     f.setLayout(null);
+    panel.setLayout(null);
     f.setVisible(true);
+  }
+
+  /*
+   * @param JButton to generate the buttons on the GUI. Buttons all look the same
+   * @param xCoord
+   * @param yCoord
+   */
+  public void generateButton(JButton button, int xCoord, int yCorrd) {
+    //button.setPreferredSize(new Dimension(250, 50));
+    button.setBounds(xCoord, yCorrd, 200, 50);
+    button.setFont(new Font("Arial", Font.PLAIN, 18));
+    button.setBackground(Color.BLACK);
+    button.setForeground(Color.WHITE);
   }
 }
