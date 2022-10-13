a29a586f25f784c54d27c129480a97cbf173a965
sydney-beeler <sydney.beeler@tamu.edu>
2022-10-13 14:36:45 -0500

    Inventory display updates

--- src/main/java/edu/tamu/spinnstone/ui/Inventory.java
+++ src/main/java/edu/tamu/spinnstone/ui/Inventory.java
@@ -59,7 +59,7 @@ public class Inventory {
 
                 dataRow[0] = product_data.getString("product_name");
                 dataRow[1] = product_data.getString("quantity_in_stock");
-                dataRow[2] = product_data.getString("conversion_factor");
+                dataRow[2] = product_data.getInt("conversion_factor") * product_data.getInt("quantity_in_stock") + " oz";
 
                 dataToDisplay.add(dataRow);
 
@@ -69,7 +69,7 @@ public class Inventory {
         }
 
         // Initializing the JTable
-        String[] columnNames = {"Product Name", "Quantity in Stock", "Conversion Factor"};
+        String[] columnNames = {"Product Name", "Quantity in Stock", "Total Amount"};
         String[][] dataToDisplayArray = dataToDisplay.toArray(new String[dataToDisplay.size()][]);
         InventoryTable = new JTable(dataToDisplayArray, columnNames);
 
