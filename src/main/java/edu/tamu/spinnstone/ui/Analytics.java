f61b501283adda654f2db3964aebce16e44c09d5
sydney-beeler <sydney.beeler@gmail.com>
2022-10-17 15:31:37 -0500

    Refactoring and order options page fix

--- src/main/java/edu/tamu/spinnstone/ui/Analytics.java
+++ src/main/java/edu/tamu/spinnstone/ui/Analytics.java
@@ -95,8 +95,7 @@ public class Analytics {
 
         try {
             // get orders between start date and end date (inclusive)
-            ResultSet rs_orders = database.query("select order_id from \"order\" " +
-                    "where (order_date <= \'" + endDate.toLocalDate() + "\' and order_date >= \'" + startDate.toLocalDate() + "\');");
+            ResultSet rs_orders = database.query("select order_id from \"order\" " + "where (order_date <= \'" + endDate.toLocalDate() + "\' and order_date >= \'" + startDate.toLocalDate() + "\');");
 
             // display no result if no orders in given range
             if (rs_orders == null) {
@@ -108,9 +107,7 @@ public class Analytics {
             // loop through orders to record pairs
             do {
                 String orderId = rs_orders.getString(1);
-                ResultSet rs_order_items = database.query("select item_name from order_item " +
-                        "join menu_item on menu_item.menu_item_id=order_item.menu_item_id " +
-                        "where order_item.order_id = " + orderId + ";");
+                ResultSet rs_order_items = database.query("select item_name from order_item " + "join menu_item on menu_item.menu_item_id=order_item.menu_item_id " + "where order_item.order_id = " + orderId + ";");
 
                 // pair items in order and record
                 ArrayList<String> items = new ArrayList<>();
